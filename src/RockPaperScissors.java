85c9c91fd588116eb67988335afe9aaddad77ff8
Jayasri <Jayasri.Sundararajan.2022@student.lhschools.org>
2018-10-29 14:16:46 -0400

    Created variables

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Random;
+public class RockPaperScissors {
+
+    public static void main(String[] args){
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        int random_pickup;
+        Random rand_new = new Random();
+        String answer;
+        answer = keyboard.nextLine();
+        int points  = 1;
+        int round = 0;
+
+        System.out.println("Hello, welcome to the rock paper scissors game");
+
+        do { System.out.println("Pick Rock, Paper, or Scissors");
+             round = round +1;
+            do {
+
+
+        }while(round == 3);
+
+   }
+}
