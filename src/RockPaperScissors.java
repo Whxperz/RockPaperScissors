886692b5dd082a93fecae83a74f48f751097f738
Jayasri <Jayasri.Sundararajan.2022@student.lhschools.org>
2018-11-01 14:19:30 -0400

    Computer vs. Player

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -6,20 +6,39 @@ public class RockPaperScissors {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
         int random_pickup;
+         String[] options = {"Rock", "Paper", "Scissors"};
         Random rand_new = new Random();
+        String computerChoice;
         String answer;
-        answer = keyboard.nextLine();
+
+
+
+        random_pickup = rand_new.nextInt(3);
+
+
         int points  = 1;
         int round = 0;
 
-        System.out.println("Hello, welcome to the rock paper scissors game");
+        do {
 
-        do { System.out.println("Pick Rock, Paper, or Scissors");
+            System.out.println("Pick Rock, Paper, or Scissors");
+            answer = keyboard.nextLine();
+            random_pickup = rand_new.nextInt(3);
+            computerChoice = options[random_pickup];
+            System.out.println(computerChoice);
              round = round +1;
-            do {
+if(answer.equalsIgnoreCase(computerChoice)){
+    System.out.println();
+}
+else if(answer.equalsIgnoreCase("rock")){
+    if()
+}
 
 
-        }while(round == 3);
+
+
+
+        }while(round < 3);
 
    }
 }
